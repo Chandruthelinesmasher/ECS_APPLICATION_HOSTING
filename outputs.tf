b314eeeb4f9e0f26d4b56f3061288d3f6db18cff
chandru <kumaravelchandru982@gmail.com>
2026-06-18 15:47:36 +0530

    modified the outputs.tf file

--- outputs.tf
+++ outputs.tf
@@ -22,13 +22,3 @@ output "secrets_manager_secret_arn" {
   description = "The ARN of the Secrets Manager secret"
   value       = aws_secretsmanager_secret.app_secrets.arn
 }
-
-output "terraform_state_bucket_name" {
-  description = "The name of the S3 bucket storing Terraform remote state"
-  value       = aws_s3_bucket.terraform_state.id
-}
-
-output "terraform_locks_table_name" {
-  description = "The name of the DynamoDB table handling state locking"
-  value       = aws_dynamodb_table.terraform_locks.name
-}
